e037ba4adeddb502f72e7c7923a3023f58c00d39
RFrost777 <tberrang@t-online.de>
2022-06-19 17:00:35 +0200

    add build target "asm" fixed segfault with elf64 binary, code cleanup, update comments
    Signed-off-by: RFrost777 <tberrang@t-online.de>

--- Makefile
+++ Makefile
@@ -9,5 +9,11 @@ debug:
 	g++ -Wall input.cpp -g -o ./bin/input_dbg
 	g++ -Wall types.cpp -g -o ./bin/types_dbg
 
+asm:
+	nasm -f elf64 -o ./bin/hello.o hello.asm
+	ld -e "main" -o ./bin/hello ./bin/hello.o
+	nasm -f elf64 -o ./bin/hello64.o hello64.asm
+	ld -o ./bin/hello64 ./bin/hello64.o
+
 clean:
 	rm ./bin/*
--- hello.asm
+++ hello.asm
@@ -1,15 +1,12 @@
 ; Hello World Assembly Version (Intel-x86)
-; using Linux syscalls 0x04 (write), 0x01 (exit)
+; using POSIX syscalls 0x04 (write), 0x01 (exit)
 ; TODO: write an IA-x64 version of this...
 
-SECTION .DATA
-  helloText:      db 'This is a write() syscall!', 10  ; Output, followed by "\n" character
-  helloLen:       equ $-helloText                      ; get sizeof(helloText), needed for write() later
+global main        ; define entry label
 
-SECTION .TEXT
-  GLOBAL _main        ; define entry label
+section .text
 
-_main:
+main:
   ; "man 2 syscall" and "man 2 write" to find out how to invoke write()
   mov eax, 4          ; invoke write() syscall
   mov ebx, 1          ; set filedesc to STDOUT(1)
@@ -21,3 +18,8 @@ _main:
   mov eax, 1          ; invoke exit() syscall
   mov ebx, 0          ; set return value (0 = OK)
   int 80h             ; request termination from kernel
+
+section .data
+
+    helloText:      db 'This is a write() syscall!', 10  ; Output, followed by "\n" character
+    helloLen:       equ $-helloText                      ; get sizeof(helloText), needed for write() later
